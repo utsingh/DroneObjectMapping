fixed yaw rotation for object detection issue

main.py
@@ -9,26 +9,14 @@ import math
 from geojson import Point
 import geopy.distance
 
-def free_norm(vector):
-    temp = np.array([[0,0],[vector[0,1]-vector[0,0],vector[1,1]-vector[1,0]]])
-    return np.linalg.norm(temp)
-
 def dist_between_lat_long_points(p1,p2):
     return geopy.distance.vincenty(p1, p2).m
-
-def scale_vector(origin, point, scalar):
-    '''Scale a vector that isn't through origin'''
-    temp = np.array([[0,0],[point[0]-origin[0],point[1]-origin[1]]])
-    scaled = scalar * temp / free_norm(temp)
-    scaled[:,0] += origin[0]
-    scaled[:,1] += origin[1]
-    return scaled
 
 def rotate(origin, point, angle):
     '''Rotate a vector that isn't thorugh origin'''
     new_x = origin[0] + math.cos(angle) * (point[0] - origin[0]) - math.sin(angle) * (point[1] - origin[1])
     new_y = origin[1] + math.sin(angle) * (point[0] - origin[0]) + math.cos(angle) * (point[1] - origin[1])
-    return (new_x, new_y)
+    return [new_x, new_y]
 
 earth_radius = 6371000.0
 def add_meters_to_lat(lat,dist):
@@ -39,6 +27,24 @@ def meters_to_lat(dist):
     return (dist / earth_radius) * (180 / np.pi)
 def meters_to_long(lat,dist):
     return (dist / earth_radius) * (180 / np.pi) / np.cos(lat * np.pi/180)
+
+def frame_point_to_rotation(point):
+    return yaw_adjust, pitch_adjust
+
+def hover_point_to_lat_long(lat,long,height,yaw,pitch):
+    '''
+    Given Drone latitude, longitude, height, pitch, and yaw, finds
+    latitude and longitude of point on ground in focus of camera view
+    '''
+    if pitch > 85:
+        pitch = 85
+    x_dist = np.tan(math.radians(pitch))*height
+    if pitch < 0:
+        return rotate((flight_points[0][-1],flight_points[1][-1]),(flight_points[0][-1],
+                add_meters_to_lat(flight_points[1][-1],x_dist)),math.radians(yaw))
+    else:
+        return rotate((flight_points[0][-1],flight_points[1][-1]),(flight_points[0][-1],
+        add_meters_to_lat(flight_points[1][-1],x_dist)),math.radians(yaw))
 
 
 # Read flight log into dataframe
@@ -93,16 +99,24 @@ video = cv2.VideoCapture('flight_footage.mp4')
 FOV = 78.8 # DJI Mavic Pro Field of View
 height_width_ratio = 3/4 # DJI Mavic Pro Ratio
 
-# Get frames per second info from video
+# Get frames per second and frame size from video
 (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 if int(major_ver) < 3 :
     fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+    frame_width = video.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
+    frame_height = video.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
 else:
     fps = video.get(cv2.CAP_PROP_FPS)
+    frame_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    frame_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+frame_center = (frame_width/2,frame_height/2)
+degree_per_width = FOV / frame_width
+degree_per_height = height_width_ratio * FOV / frame_height
 
 # Create interactive plot and limit to flight area
-plt.xlim(gps_x_min,gps_x_max)
-plt.ylim(gps_y_min,gps_y_max)
+plt.xlim(gps_x_min-.001,gps_x_max+.001)
+plt.ylim(gps_y_min-.001,gps_y_max+.001)
 plt.ion()
 plt.show()
 
@@ -110,6 +124,9 @@ time = 0
 flight_points = [[f_x_pos(time)],[f_y_pos(time)]]
 i = 1
 while True:
+    flight_points[0].append(float(f_x_pos(time)))
+    flight_points[1].append(float(f_y_pos(time)))
+    current_height = np.round(f_height(time),2)
     object_map_points = []
     if f_record(time) == 1: # If camera is recording, show frame
         success, frame = video.read()
@@ -119,93 +136,46 @@ while True:
         # if cv2.waitKey(1) == 27:
         #     break
 
+    if g_pitch(time) < 0:
+        view_pitch = 90 + g_pitch(time)
+        dist_to_focus = current_height / np.cos(math.radians(view_pitch))
+
+        drone_focus_point = hover_point_to_lat_long(flight_points[0][-1],flight_points[1][-1],
+                                                    current_height,-g_yaw(time),view_pitch)
+
+        '''Insert NN for detection of objects in frame'''
+        detections = [(0,0),(frame_width,0),(frame_width,frame_height),(0,frame_height)]
+
+        detection_points = []
+        for detection in detections:
+            pitch_rotation = (detection[1] - frame_center[1]) * degree_per_height
+            yaw_rotation = (detection[0] - frame_center[0]) * degree_per_width
+            if (view_pitch + pitch_rotation) < 0:
+                yaw_rotation = -yaw_rotation
+            point = hover_point_to_lat_long(flight_points[0][-1],flight_points[1][-1],
+                    current_height,-g_yaw(time) + yaw_rotation,view_pitch + pitch_rotation)
+
+            detection_points.append(point)
+
+        detection_lines = []
+        detection_lines.append(plt.plot([detection_points[0][0],detection_points[1][0]],[detection_points[0][1],detection_points[1][1]],'red'))
+        detection_lines.append(plt.plot([detection_points[1][0],detection_points[2][0]],[detection_points[1][1],detection_points[2][1]],'red'))
+        detection_lines.append(plt.plot([detection_points[2][0],detection_points[3][0]],[detection_points[2][1],detection_points[3][1]],'red'))
+        detection_lines.append(plt.plot([detection_points[3][0],detection_points[0][0]],[detection_points[3][1],detection_points[0][1]],'red'))
+        #
+        plt.plot(flight_points[0][-2:],flight_points[1][-2:],'blue')
+        drone_fp = plt.scatter(drone_focus_point[0],drone_focus_point[1],c='orange')
+        plt.pause(.001)
+        drone_fp.remove()
+        for line in detection_lines:
+            line[0].remove()
+
+    # else:
+    #     plt.plot(flight_points[0][-2:],flight_points[1][-2:],'blue')
+    #     plt.pause(.001)
+
     # time += 1/fps
-    time += 1
-    flight_points[0].append(float(f_x_pos(time)))
-    flight_points[1].append(float(f_y_pos(time)))
-
-    current_height = np.round(f_height(time),2)
-    # print('Pitch: {}, Yaw: {}, Dist: {}'.format(np.round(g_pitch(time),2),np.round(g_yaw(time),2),current_height))
-    if g_pitch(time) < -FOV/2:
-        theta = 90 + g_pitch(time)
-        dist_to_focus = current_height/np.cos(90-theta)
-        x_dist = np.tan(math.radians(theta))*current_height
-        drone_focus_point = rotate((flight_points[0][-1],flight_points[1][-1]),(flight_points[0][-1],add_meters_to_lat(flight_points[1][-1],x_dist)),math.radians(-g_yaw(time)))
-
-        # ground_vector = np.array([[flight_points[0][-1],flight_points[1][-1]],[drone_focus_point[0],drone_focus_point[1]]])
-
-        # Find transformation
-        # pts_src = np.array([[int(frame.shape[1]/2), int(frame.shape[0]/2)], # Middle
-        #                     [0, int(frame.shape[0]/2)], # Middle Left
-        #                     [frame.shape[1],int(frame.shape[0]/2)], # Middle Right
-        #                     [int(frame.shape[1]/2), 0], # Top Middle
-        #                     [int(frame.shape[1]/2), frame.shape[0]]]) # Bottom Middle
-        # horizontal_view_dist = np.tan(FOV/2) * current_height / np.cos(theta)
-        # vertical_view_dist_up = np.sin(FOV/2) * dist_to_focus / np.sin(90 - theta - FOV/2)
-        # vertical_view_dist_down = np.sin(FOV/2) * dist_to_focus / np.sin(90 - theta)
-
-        # view_extension_up = vertical_view_dist_up * np.tan(90 - theta)
-        # view_extension_down = vertical_view_dist_down * np.tan(90 - theta)
-
-        # pts_dst = np.array([[drone_focus_point[0], drone_focus_point[1]],
-        #                     [489, drone_focus_point[1]],
-        #                     [505, drone_focus_point[1]],
-        #                     [drone_focus_point[0], 235],
-        #                     [drone_focus_point[0],153]])
-        # h, status = cv2.findHomography(pts_src, pts_dst)
-        # Detection flight_points
-        # a = np.array([[int(frame.shape[1]/2), int(frame.shape[0]/2)]], dtype='float32')
-        # a = np.array([a])
-        # Map flight_points
-        # map_points = cv2.perspectiveTransform(a, h)
-        map_points = [drone_focus_point]
-
-        plt.plot(flight_points[0][-2:],flight_points[1][-2:],'blue')
-        for point in map_points:
-            object_map_points.append(plt.scatter(point[0],point[1],c='orange'))
-
-        # rotate_point1 = rotate(ground_vector[1],ground_vector[0],math.radians(90))
-        # rotate_point2 = rotate(ground_vector[1],ground_vector[0],math.radians(270))
-        # rotate_point3 = rotate(ground_vector[1],ground_vector[0],math.radians(0))
-        # rotate_point4 = rotate(ground_vector[1],ground_vector[0],math.radians(180))
-        #
-        # left_vector = np.array([[ground_vector[1][0],rotate_point1[0]],[ground_vector[1][1],rotate_point1[1]]])
-        # right_vector = np.array([[ground_vector[1][0],rotate_point2[0]],[ground_vector[1][1],rotate_point2[1]]])
-        # down_vector = np.array([[ground_vector[1][0],rotate_point3[0]],[ground_vector[1][1],rotate_point3[1]]])
-        # up_vector = np.array([[ground_vector[1][0],rotate_point4[0]],[ground_vector[1][1],rotate_point4[1]]])
-        #
-        # corner_point1 = rotate(up_vector[:,1],up_vector[:,0],math.radians(270))
-        # corner_vector1 = np.array([[up_vector[0][1],corner_point1[0]],[up_vector[1][1],corner_point1[1]]])
-        # normed_extension_up = free_norm(corner_vector1) * view_extension_up / dist_between_lat_long_points(corner_vector1[:,0],corner_vector1[:,1])
-        # top_left_vector = scale_vector(corner_vector1[:,0],corner_vector1[:,1],free_norm(corner_vector1)+normed_extension_up)
-        #
-        # corner_point2 = rotate(down_vector[:,1],down_vector[:,0],math.radians(90))
-        # corner_vector2 = np.array([[down_vector[0][1],corner_point2[0]],[down_vector[1][1],corner_point2[1]]])
-        # normed_extension_down = free_norm(corner_vector2) * view_extension_down / dist_between_lat_long_points(corner_vector2[:,0],corner_vector2[:,1])
-        # bottom_left_vector = scale_vector(corner_vector2[:,0],corner_vector2[:,1],free_norm(corner_vector2)+normed_extension_down)
-        #
-        # c = plt.plot(left_vector[0],left_vector[1],'blue')
-        # d = plt.plot(right_vector[0],right_vector[1],'blue')
-        # e = plt.plot(up_vector[0],up_vector[1],'blue')
-        # f = plt.plot(down_vector[0],down_vector[1],'blue')
-        # c2 = plt.plot(top_left_vector[:,0],top_left_vector[:,1],'red')
-        # d2 = plt.plot(bottom_left_vector[:,0],bottom_left_vector[:,1],'red')
-
-        plt.pause(.001)
-        for point in object_map_points:
-            point.remove()
-        # c[0].remove()
-        # d[0].remove()
-        # e[0].remove()
-        # f[0].remove()
-        # c2[0].remove()
-        # d2[0].remove()
-
-    else:
-        plt.plot(flight_points[0][-2:],flight_points[1][-2:],'blue')
-        plt.pause(.001)
-
-
+    time += .8
     i += 1
 # cv2.destroyAllWindows()
 